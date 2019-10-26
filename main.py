400116470d81309271f29883ddad4710dd63be0d
Arnav Kumar <arnavpisces@gmail.com>
2019-10-26 23:05:57 +0530

    custom geo json

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+
+
 app=Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
+
+@app.route('/points/')
+def return_files_tut():
+	try:
+		return send_file('json/geo.json', attachment_filename='geo.json')
+	except Exception as e:
+		return str(e)
 
 if __name__=='__main__':
     app.run(debug=True,threaded=True)
