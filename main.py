0f16c10d32a042e99d8a8603635405b444b803e6
Arnav Kumar <arnavpisces@gmail.com>
2019-10-26 08:20:11 +0530

    sample map app deploy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def index():
-    return "<h1>Hello Azure!</h1>"
+    return render_template("index.html")
+
+if __name__=='__main__':
+    app.run(debug=True,threaded=True)
 
