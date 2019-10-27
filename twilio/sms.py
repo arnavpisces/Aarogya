72f5d4062ca5cf0667233724d81c1fef04a2ba7b
Arnav Kumar <arnavpisces@gmail.com>
2019-10-27 16:44:46 +0530

    fixed reqs

--- twilio/sms.py
+++ twilio/sms.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC64f5421baf4500a207dfd6f54752d9df"
+account_sid = "AC3c1060cf9c5700d98029dcd2e8693d16"
 # Your Auth Token from twilio.com/console
-auth_token  = "21b55a570d845eaed3fa1e0d4aa92043"
+auth_token  = "6f3ad24140d08f40c2b6c85531e3f048"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+917838816965", 
-    from_="+12014654883",
-    body="Hello from Python!")
+    to="+12014654883", 
+    from_="+12057032768",
+    body="location")
 
 print(message.sid)
